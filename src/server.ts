d3ee92d52334d50728e3197bb4b6ee51ac2a6c6a
RJDM15 <ricardoj.dm122@gmail.com>
2026-03-29 23:24:57 -0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,46 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL || "*",
+    credentials: true,
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.json({ message: "API Working" });
+});
+
+if (!process.env.MONGODB_URI) {
+    throw new Error("Please provide a MongoDB URI");
+}
+
+export const mongoClient = new MongoClient(process.env.MONGODB_URI);
+
+async function start() {
+    try {
+        await mongoClient.connect();
+        console.log("MongoDB conectado");
+
+        app.listen(PORT, () => {
+            console.log(`Servidor corriendo en el puerto ${PORT}`);
+        });
+
+    } catch (error) {
+        console.error("Error al conectar a la base de datos:", error);
+        process.exit(1)
+    }
+}
+
+start();
+
+export default app;
